000d6bda3ed7e40d0dba39202556c06601147889
Adrian <Adrian@LAPTOP-IIUB3F29>
2017-09-13 20:13:22 +0200

    Städat main och flyttat det som var i till en egen metod. Gjort så det inte längre är en loop då det bara ska vara en engage per gång man möter Draken

---
+++ CombatSystem/Combat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CombatSystem
+{
+    class Combat
+    {
+        public static void Drakfight()
+        {
+            int lifeKnight = 100;
+            int damage;
+            if(lifeKnight > 0)
+            {
+                Style combat = new Style();
+                string dragonStyle = Style.BattleStance("Dragon");
+                if (dragonStyle == "Agressive")
+                    Console.WriteLine("The Dragon Looks agressive");
+                if (dragonStyle == "Standard")
+                    Console.WriteLine("The Dragon looks calculative");
+                if (dragonStyle == "Defensive")
+                    Console.WriteLine("The Dragon looks startled");
+
+
+                Console.WriteLine("choose what to do! Dodge(1) Run(2) Defend with shield(3)");
+                string knightStyle = Style.BattleStance(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.WriteLine("The Dragon attacks!");
+
+                damage = Style.Damage(Style.StyleSwitch(dragonStyle, knightStyle));
+                lifeKnight = Life.DamageDone(damage, lifeKnight);
+                Console.WriteLine("Knight has " + lifeKnight + " life remaining");
+                Console.WriteLine();
+                
+            }
+            if (lifeKnight <= 0)
+            {
+                Console.WriteLine("The knight got crushed, stomped and devoured by the Dragon");
+
+            }
+        }
+    }
+}
--- CombatSystem/Program.cs
+++ CombatSystem/Program.cs
@@ -7,62 +7,21 @@ namespace CombatSystem
     public class Program
     {
         public static void Main(string[] args)
-        {
-            int lifeDragon = 100;
-            int lifeKnight = 100;
-            int damage;
-            while (lifeDragon > 0 || lifeKnight > 0)
-            {
-                Style combat = new Style();
-                //string knightStyle = Style.BattleStance();
+        {    /* här försöker jag typ simulera vad som skulle hända
+               vad som skulle hända om man går in i en array ruta
+               där draken finns*/
+            Combat fight = new Combat();
+            bool attacked = false;
+            int test;
+            Console.WriteLine("press 1 to start an engagement");
+            test = int.Parse(Console.ReadLine());
 
-                //Console.WriteLine("Knights stance is " + knightStyle);
+            if (test == 1)
+                attacked = true;
 
-                string dragonStyle = Style.BattleStance("Dragon");
-                if (dragonStyle == "Agressive")
-                    Console.WriteLine("The Dragon Looks agressive");
-                if (dragonStyle == "Standard")
-                    Console.WriteLine("The Dragon looks calculative");
-                    if (dragonStyle == "Defensive")
-                    Console.WriteLine("The Dragon looks startled");
+            if(attacked == true)
+            Combat.Drakfight();
 
-
-                        Console.WriteLine("choose what to do! Dodge(1) Run(2) Defend with shield(3)");
-                string knightStyle = Style.BattleStance(Console.ReadLine());
-
-                //damage = Style.Damage(Style.StyleSwitch(knightStyle, dragonStyle));
-                //lifeDragon = Life.DamageDone(damage, lifeDragon);
-                //Console.WriteLine("Dragon has " + lifeDragon + " life remaining");
-                //if (lifeDragon <= 75 && lifeDragon >= 51)
-                //    Console.WriteLine("Foolish human, I will burn you alive!");
-
-                //if (lifeDragon <= 50 && lifeDragon >= 26)
-                //    Console.WriteLine("GRARARRRARARAR, YOU ANGER ME!");
-
-                //if (lifeDragon <= 25 && lifeDragon >= 0)
-                //    Console.WriteLine(".... WHAT IS THIS!?!");
-                Console.WriteLine();
-                Console.WriteLine("The Dragon attacks!");
-
-                damage = Style.Damage(Style.StyleSwitch(dragonStyle, knightStyle));
-                lifeKnight = Life.DamageDone(damage, lifeKnight);
-                Console.WriteLine("Knight has " + lifeKnight + " life remaining");
-                Console.WriteLine();
-
-                if (lifeKnight <= 0)
-                {
-                    Console.WriteLine("The knight got crushed, stomped and devoured by the Dragon");
-                    break;
-                }
-
-
-                if (lifeDragon <= 0)
-                {
-                    Console.WriteLine("The knight bested the Dragon!");
-                    break;
-                }
-            }
-            //Console.WriteLine(styleResult);
             Console.Read();
 
 
